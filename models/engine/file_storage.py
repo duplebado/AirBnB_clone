a35d52301d9b433565651623b57ca9ee91cb4e93
duplebado <ajibadeadebayoengr@gmail.com>
2021-12-28 13:57:21 +0000

    Debug atempt

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -58,6 +58,7 @@ class FileStorage:
         try:
             with open(self.__file_path, encoding="utf-8") as f:
                 for obj in json.load(f).values():
+                    print("obj", obj)
                     self.new(eval(obj["__class__"])(**obj))
         except:
             pass
