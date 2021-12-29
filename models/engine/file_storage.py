9fb00632c6c7cc55e634a35c2453d9070979d3de
duplebado <ajibadeadebayoengr@gmail.com>
2021-12-29 10:32:16 +0000

    Debug

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -59,6 +59,7 @@ class FileStorage:
             with open(self.__file_path, encoding="utf-8") as f:
                 stored_dict = json.load(f)
                 print("length of stored_dict", len(stored_dict))
+                print("stored_dict ", stored_dict)
                 for obj in stored_dict.values():
                     self.new(json.loads(obj))
                 print("length of objs", len(self.__objects))
