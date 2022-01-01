cfb60f62695456527c1a080b8c730ceea821ca23
duplebado <ajibadeadebayoengr@gmail.com>
2022-01-01 11:13:21 +0000

    Complete task 10

--- console.py
+++ console.py
@@ -5,6 +5,12 @@
 import cmd
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.state import State
+from models.review import Review
 
 ### an array of classes in the system
 CLASSES = [
