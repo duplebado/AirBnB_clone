91faf5d02f351b2a714b219c2b86318430ff64cc
duplebado <ajibadeadebayoengr@gmail.com>
2022-01-01 07:59:32 +0000

    Completed task 7

--- console.py
+++ console.py
@@ -128,15 +128,36 @@ class HBNBCommand(cmd.Cmd):
         arg_list = argv.split()
 
         if len(arg_list) == 0:
-            print([obj.__str__() for obj in storage.all().items()])
+            print([obj.__str__() for obj in storage.all().values()])
         elif arg_list[0] in CLASSES:
-            for obj in storage.all().items():
-                print(vars(obj))
-                print("class names ", obj.__class__.__name__)
-            print([obj.__str__() for obj in storage.all().items() if obj.__class__.__name__ == arg_list[0]])
+            print([obj.__str__() for obj in storage.all().values() if obj.__class__.__name__ == arg_list[0]])
         else:
             print("** class doesn't exist **")
 
+    def do_update(self, argv):
+        """
+            Updates an instance based on the class name and id by
+            adding or updating attribute
+        """
+        args = check_args(argv)
+
+        if len(args) == 2:
+            print("** attribute name missing **")
+        if args[2] == "id" or args[2] == "created_at" or args[2] == "updated_at":
+            pass
+        elif len(args) == 3:
+            print("** value missing **")
+        else:
+            key = "{}.{}".format(args[0], args[1])
+            obj = storage.all()[key]
+            
+            if args[2] in type(obj).__dict__:
+                v_type = type(obj.__class__.__dict__[args[2]])
+                setattr(obj, args[2], v_type(args[3]))
+            else:
+                setattr(obj, args[2], args[3])
+
+            obj.save()
 
 
 if __name__ == "__main__":
